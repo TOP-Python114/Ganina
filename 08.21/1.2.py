13dfe5862fcbf73a8890da9de04760f58e4edd3d
Gennadiy <renardes@gmail.com>
2022-09-03 10:20:57 +0500

    исправления, комментарии, итоги

--- 08.21/1.2.py
+++ 08.21/1.2.py
@@ -13,19 +13,31 @@
 import itertools
 
 
+# ИСПРАВИТЬ: аннотации и документация обязательны
 def permutations(set_p):
-    list_var = []
-    perm_list = list(itertools.permutations(set_p))
-    for elen_perm_list in perm_list:
-        elem_list_var = ''
-        if len(elen_perm_list) > 0:
-            for i in range(0, len(elen_perm_list)):
-                elem_list_var += elen_perm_list[i]
-            list_var.append(elem_list_var)
-    for j in range(0, len(list_var)):
-        yield list_var[j]
+    # ИСПОЛЬЗОВАТЬ: не нужен ещё один список, если используете itertools.permutations()
+    # list_var = []
+
+    # ИСПРАВИТЬ: вот это никуда не годится — где строковые методы, где генераторные выражения?
+    # perm_list = list(itertools.permutations(set_p))
+    # for elen_perm_list in perm_list:
+    #     elem_list_var = ''
+    #     if len(elen_perm_list) > 0:
+    #         for i in range(0, len(elen_perm_list)):
+    #             elem_list_var += elen_perm_list[i]
+    #         list_var.append(elem_list_var)
+    # for j in range(0, len(list_var)):
+    #     yield list_var[j]
+
+    # ИСПОЛЬЗОВАТЬ:
+    for perm in itertools.permutations(set_p):
+        yield ''.join(str(elem) for elem in perm)
 
 
+# ДОБАВИТЬ: за знание стандартной библиотеки хвалю, но предполагалось, что вы всё-таки напишите свой генератор перестановок
+
+
+# СДЕЛАТЬ: добавьте во множество объект int и посмотрите, что получится
 set_per = {'a', 'b', 'c'}
 per = permutations(set_per)
 for n in per:
@@ -40,3 +52,5 @@ cab
 abc
 acb
 """
+
+# ИТОГ: можно лучше — 2/6
